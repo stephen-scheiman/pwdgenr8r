636805755aa3b2d2935eb1307b71ee024b22aeb6
stephen-scheiman <sbscheiman@gmail.com>
2023-11-10 15:52:57 -0800

    Updated README, tweaked special character set

--- script.js
+++ script.js
@@ -50,7 +50,8 @@ useNumeric = window.prompt("Would you like to include UPPER CASE characters? Y o
 let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let numeric = [0,1,2,3,4,5,6,7,8,9];
-let symbols = ["!","@","#","$","%","^","&","*","(",")","-","+","=","_"];
+let strSymbols = `\`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~";'`;
+let symbols = strSymbols.split("");
 let characterPool = [];
 let charSelector;
 let buildtmppwd = [];
